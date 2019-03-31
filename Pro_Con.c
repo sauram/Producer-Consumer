ea395f73db34f1cb42343655568a4adaa76f398b
Saurabh Singh Gautam <32809271+sauram@users.noreply.github.com>
2019-03-31 11:29:29 +0530

    Update Pro_Con.c

--- Pro_Con.c
+++ Pro_Con.c
@@ -9,15 +9,15 @@ Following is the solution of Producer-Consumer Problem
 #include<stdio.h>
 #include<stdlib.h>
  
-int mutex=1,full=0,empty=3,x=0;
+int mutual_wala_exclusion=1,Buffer_is_full=0,Buffer_is_empty=3,x_pc=0;
  
 int main()
 {
 	int n;
-	void producer();
-	void consumer();
-	int wait(int);
-	int signal(int);
+	void smproducerfn();
+	void smconsumerfn();
+	int wait_sema_phore(int);
+	int signal_sema_phore(int);
 	printf("\n1.Producer\n2.Consumer\n3.Exit");
 	while(1)
 	{
@@ -25,15 +25,15 @@ int main()
 		scanf("%d",&n);
 		switch(n)
 		{
-			case 1:	if((mutex==1)&&(empty!=0))
-						producer();
+			case 1:	if((mutual_wala_exclusion==1)&&(Buffer_is_empty!=0))
+						smproducerfn();
 					else
-						printf("Oops ! Buffer is full.");
+						printf("Oops ! Buffer is Buffer_is_full.");
 					break;
-			case 2:	if((mutex==1)&&(full!=0))
-						consumer();
+			case 2:	if((mutual_wala_exclusion==1)&&(Buffer_is_full!=0))
+						smconsumerfn();
 					else
-						printf("Oops ! Buffer is empty.");
+						printf("Oops ! Buffer is Buffer_is_empty.");
 					break;
 			case 3:
 					exit(0);
@@ -44,32 +44,32 @@ int main()
 	return 0;
 }
  
-int wait(int s)
+int wait_sema_phore(int s)
 {
 	return (--s);
 }
  
-int signal(int s)
+int signal_sema_phore(int s)
 {
 	return(++s);
 }
  
-void producer()
+void smproducerfn()
 {
-	mutex=wait(mutex);
-	full=signal(full);
-	empty=wait(empty);
-	x++;
-	printf("\nProducer produces the item %d",x);
-	mutex=signal(mutex);
+	mutual_wala_exclusion=wait_sema_phore(mutual_wala_exclusion);
+	Buffer_is_full=signal_sema_phore(Buffer_is_full);
+	Buffer_is_empty=wait_sema_phore(Buffer_is_empty);
+	x_pc++;
+	printf("\nProducer produces the item %d",x_pc);
+	mutual_wala_exclusion=signal_sema_phore(mutual_wala_exclusion);
 }
  
-void consumer()
+void smconsumerfn()
 {
-	mutex=wait(mutex);
-	full=wait(full);
-	empty=signal(empty);
-	printf("\nConsumer consumes item %d",x);
-	x--;
-	mutex=signal(mutex);
+	mutual_wala_exclusion=wait_sema_phore(mutual_wala_exclusion);
+	Buffer_is_full=wait_sema_phore(Buffer_is_full);
+	Buffer_is_empty=signal_sema_phore(Buffer_is_empty);
+	printf("\nConsumer consumes item %d",x_pc);
+	x_pc--;
+	mutual_wala_exclusion=signal_sema_phore(mutual_wala_exclusion);
 }
